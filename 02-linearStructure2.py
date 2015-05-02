01ae792d78a40e5bba3753e45c457309bd18be94
SAmmer0 <howardlee_h@outlook.com>
2015-05-02 22:18:35 +0800

    complete task

--- 02-linearStructure2.py
+++ 02-linearStructure2.py
@@ -1,69 +1,46 @@
-class LinkList:
-	def __init__(self, data, start, end):
-		self._data = data
-		self._start = start
-		self._end = end
-		self._count = len(data)
-	
-	def reverseLinkList(self):
-		tmpData = dict()
-		currentNode = self._end
-		lastNode = None
-		while True:
-			tmpDict = {'address': currentNode,
-						'value': self._data[currentNode]['value'],
-						'last': lastNode,
-						'next': self._data[currentNode]['last']}
-			tmpData[currentNode] = tmpDict
-			if currentNode == self._start:
-				break
-			lastNode = currentNode
-			currentNode = self._data[currentNode]['last']
-		self._data = tmpData
-		self._start, self._end = self._end, self._start
-	
-	def joinLinkList(self, nextLinkList):
-		self._data.update(nextLinkList.getData())
-		self._data[self._end]['next'] = nextLinkList.getStart()
-		self._end = nextLinkList.getEnd()
-		self._count += nextLinkList.getCount()
-	
-	def getStart(self):
-		return self._start
-	
-	def getEnd(self):
-		return self._end
-	
-	def getData(self):
-		return self._data
-	
-	def getCount(self):
-		return self._count
-	
-	def printLinkList(self):
-		currentNode = self._start
-		data = self._data
-		while True:
-			print data[currentNode]['address'] + ' ' +\
-				  data[currentNode]['value'] + ' ' +\
-				  data[currentNode]['next'] + '\n '
-		
-		
-def getInput():
-	# get initial input message
-	inputStr = raw_input()
-	startNode, num, step = inputStr.strip().split()
-	num = int(num)
-	step = int(step)
-	
-	# convert input into link list
-	linkList = dict()
-	for dummy_i in xrange(num):
-		thisInput = raw_input()
-		thisInput = thisInput.strip().split()
-		thisNode = {'address': thisInput[0], 'value': int(thisInput[1]), 'next': thisInput[2]}
-		linkList[thisInput[0]] = thisNode
-	
-	
-	
-			
+def getLinkListInput():
+    listMsg = raw_input()
+    startId, num, step = listMsg.strip().split()
+    num = int(num)
+    step = int(step)
+    dLinkList = dict()
+    for dummy_i in xrange(num):
+        thisInput = raw_input()
+        thisAddress, thisValue, nextAddress = thisInput.strip().split()
+        dLinkList[thisAddress] = {'address': thisAddress,
+                                  'value': thisValue,
+                                  'next': nextAddress}
+    
+    currentId = startId
+    resList = list()
+    while(currentId != '-1'):
+        resList.append((dLinkList[currentId]['address'], 
+                    dLinkList[currentId]['value']))
+        currentId = dLinkList[currentId]['next']
+    return resList, step
+
+def reverseList(inputList, reverseStep):
+    thisIdx = 0
+    length = len(inputList)
+    while(thisIdx + reverseStep <= length):
+        start = thisIdx
+        end = thisIdx + step - 1
+        while(end > start):
+            inputList[start], inputList[end] = inputList[end], inputList[start]
+            start += 1
+            end -= 1
+        thisIdx += reverseStep
+
+def printList(inputList):
+    length = len(inputList)
+    for idx in xrange(length):
+        if idx != length - 1:
+            print inputList[idx][0] + ' ' + inputList[idx][1] + ' ' +\
+                  inputList[idx + 1][0]
+        else:
+            print inputList[idx][0] + ' ' + inputList[idx][1] + ' ' +\
+                  '-1'    
+if __name__ == '__main__':
+    linkList, step = getLinkListInput()
+    reverseList(linkList, step)
+    printList(linkList)
